80db09d82819d8cd7eb0be19ccef36f6dd0a8533
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-12-14 17:04:22 +0100

    modified:   package-lock.json         modified:   package.json         modified:   public/index.html         modified:   src/components/Header.js         modified:   src/components/Markdown.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function () {
     return (
         <div className='d-flex justify-content-center align-items-center'>
-            <h2>Note taking app</h2>
+            <h2>Note taking app <i class="fas fa-edit text-primary"></i></h2>
         </div>
     )
 }
--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
+import MDEditor from '@uiw/react-md-editor';
 
 export default function Markdown() {
+    const [value, setValue] = React.useState("**Hello world!!!**");
     return (
         <div className='col-5 m-2 border border-1 border-secondary p-2'>
             <div className='text-dark bg-white'>
-                <h3>Hello World</h3>
+            <MDEditor value={value} onChange={setValue} />
+
+            <MDEditor.Markdown source={value} />
             </div>
         </div>
     )
