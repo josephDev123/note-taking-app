e0b6f0257b5d907ca427624cbe32cb1d86b97ffd
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-12-21 15:56:06 +0100

    modified:   src/App.css         modified:   src/components/NoteList.js

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -9,10 +9,15 @@ export default function NoteList() {
         let noteList = notes.map((item ,i) => 
         // col-3 m-2 border border-1 border-secondary p-2
                     <Fragment key={item.id}>
-                        <div className="card m-2 border border-1 border-secondary p-2">
+                        <div className="card m-2 border border-1 border-secondary p-2 position-relative">
                             <div className="card-body">
                                 <h5 className="card-title">Note: {item.id.substr(0, 8)}</h5>
                                 <p className="card-text">{item.note.substr(0, 25).concat('....')}.</p>
+                            </div>
+                            <div className='position-absolute d-flex justify-content-between top-0 end-0'>
+                                <span className='me-3 icon'><i className="far fa-star fa-xs"></i></span>
+                                <span className='me-3 icon'><i className="fas fa-trash fa-xs"></i></span>
+                                <span style={{ fontSize:'7px' }} className='text-danger fs-6 me-2 fw-bold icon'>X</span>
                             </div>
                         </div>
                     </Fragment>   
